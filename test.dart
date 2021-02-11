c9b14b9d43d3d5e56ae719a2584dd57253a56d19
N-M-Joli <35491683+N-M-Joli@users.noreply.github.com>
2021-02-11 15:38:53 +0100

    2st commit

--- test.dart
+++ test.dart
@@ -1,8 +1,102 @@
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ExampleScreen(),
-    ),
-  );
+List<String> messages = [];
+bool logoutSucceeds = false;
+const passed = 'PASSED';
+const noCatch = 'NO_CATCH';
+const typoMessage = 'Test failed! Check for typos in your return value';
+const oneSecond = Duration(seconds: 1);
+
+class UserError implements Exception {
+  String errMsg() => 'New username is invalid';
+}
+
+Future fetchNewUsername() {
+  var str = Future.delayed(oneSecond, () => throw UserError());
+  return str;
+}
+
+main() async {
+  try {
+    // ignore: cascade_invocations
+    messages
+      ..add(makeReadable(
+          testLabel: '',
+          testResult: await asyncDidCatchException(changeUsername),
+          readableErrors: {
+            typoMessage: typoMessage,
+            noCatch:
+                'Did you remember to call fetchNewUsername within a try/catch block?',
+          }))
+      ..add(makeReadable(
+          testLabel: '',
+          testResult: await asyncErrorEquals(changeUsername),
+          readableErrors: {
+            typoMessage: typoMessage,
+            noCatch:
+                'Did you remember to call fetchNewUsername within a try/catch block?',
+          }))
+      ..removeWhere((m) => m.contains(passed))
+      ..toList();
+
+    if (messages.isEmpty) {
+      _result(true);
+    } else {
+      _result(false, messages);
+    }
+  } catch (e) {
+    _result(false, ['Tried to run solution, but received an exception: $e']);
+  }
+}
+
+////////////////////////////////////////
+///////////// Test Helpers /////////////
+////////////////////////////////////////
+String makeReadable({String testResult, Map readableErrors, String testLabel}) {
+  if (readableErrors.containsKey(testResult)) {
+    var readable = readableErrors[testResult];
+    return '$testLabel $readable';
+  } else {
+    return '$testLabel $testResult';
+  }
+}
+
+void passIfNoMessages(List<String> messages, Map<String, String> readable) {
+  if (messages.isEmpty) {
+    _result(true);
+  } else {
+    // ignore: omit_local_variable_types
+    List<String> userMessages = messages
+        .where((message) => readable.containsKey(message))
+        .map((message) => readable[message])
+        .toList();
+    print(messages);
+
+    _result(false, userMessages);
+  }
+}
+
+///////////////////////////////////////
+//////////// Assertions ///////////////
+///////////////////////////////////////
+Future<String> asyncErrorEquals(Function fn) async {
+  var result = await fn();
+  if (result == UserError().toString()) {
+    return passed;
+  } else {
+    return 'Test failed! Did you stringify and return the caught error?';
+  }
+}
+
+Future<String> asyncDidCatchException(Function fn) async {
+  var caught = true;
+  try {
+    await fn();
+  } on UserError catch (_) {
+    caught = false;
+  }
+
+  if (caught == false) {
+    return noCatch;
+  } else {
+    return passed;
+  }
 }
